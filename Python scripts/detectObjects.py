61040fd236611137513799433a7ce72f8d41d414
aDolinar <ad5062@student.uni-lj.si>
2020-06-01 23:36:55 +0200

    successful petridish detection in notebook
    also edited the test photo

--- Python scripts/detectObjects.py
+++ Python scripts/detectObjects.py
@@ -2,5 +2,5 @@ import cv2
 import matplotlib.pyplot as plt
 import detObjFunc as RV
 from PIL import Image
-orImage=Image.open(r"petriDish_test.jpg")
-#RV.showImage(orImage,"Original Image")
+orImage=RV.loadImage(r"petriDish_test.jpg")
+RV.showImage(orImage,"Original Image")
